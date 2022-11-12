7ce980a5b27407e00289961d301f1a3814d28a72
dreax <gejuan.demby@gmail.com>
2022-11-12 08:44:12 -0600

    Created modules for the project and cleaned errors

---
+++ utils/sortStudent.js
@@ -0,0 +1,25 @@
+import { students, houses } from './studentData';
+import createId from './createID';
+import { studentsOnDom } from '../components/structure.html';
+
+const sortStudent = (e) => {
+  e.preventDefault();
+  const sortingHat = houses[Math.floor(Math.random() * houses.length)];
+
+  if (e.target.id === 'sorting') {
+    const student = document.querySelector('#student-name');
+
+    // create the new student object
+    students.push({
+      id: createId(students),
+      name: student.value,
+      house: sortingHat.house,
+      crest: sortingHat.crest
+    });
+
+    student.value = ''; // reset value of input
+    studentsOnDom('#students', students);
+  }
+};
+
+export default sortStudent;
